eb8ef95457659c51d696c2d88ba7ed7b2e4dd694
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-08-30 17:57:40 -0300

    Menus principais

--- Sistema Ecommerce/main.py
+++ Sistema Ecommerce/main.py
@@ -1,3 +1,108 @@
 # Crie um menu contendo as opções Menu Cliente e Menu Produto
 
 # Implemente as funcionalidades de CRUD para Cliente e Produto
+
+from conexaoDB import ConexaoDB
+import dotenv
+import os
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+meuBanco = ConexaoDB(dbname=DB_NAME, host=DB_HOST,
+                     port=DB_PORT, user=DB_USER, password=DB_PASSWORD)
+
+
+def menuCliente():
+    while True:
+        print("MENU CLIENTES")
+        print('''
+Operações:
+
+1. Ver Clientes
+2. Cadastrar Cliente
+3. Atualizar Cliente
+4. Remover Cliente
+
+0. Voltar para o menu principal        
+''')
+        op = input("Digite o número da opção desejada:")
+
+        if op == "1":
+            pass
+        elif op == "2":
+            pass
+        elif op == "3":
+            pass
+        elif op == "4":
+            pass
+        elif op == "0":
+            print("Voltando para o menu principal...")
+            break
+        else:
+            print("Escolha uma opção válida!")
+
+        input("TECLE ENTER PARA CONTINUAR.")
+
+
+def menuProduto():
+    while True:
+        print("MENU PRODUTOS")
+        print('''
+Operações:
+
+1. Ver Produtos
+2. Cadastrar Produto
+3. Atualizar Produto
+4. Remover Produto
+
+0. Voltar para o menu principal        
+''')
+        op = input("Digite o número da opção desejada:")
+
+        if op == "1":
+            pass
+        elif op == "2":
+            pass
+        elif op == "3":
+            pass
+        elif op == "4":
+            pass
+        elif op == "0":
+            print("Voltando para o menu principal...")
+            break
+        else:
+            print("Escolha uma opção válida!")
+
+        input("TECLE ENTER PARA CONTINUAR.")
+
+
+while True:
+    print("Boas vindas ao Ecommerce XYZ")
+
+    print('''
+Menu:
+
+1. Menu Clientes
+2. Menu Produtos
+          
+0. Sair
+''')
+    op = input("Digite o número da opção desejada:")
+
+    if op == "1":
+        menuCliente()
+    elif op == "2":
+        menuProduto()
+    elif op == "0":
+        print("Saindo do programa...")
+        break
+    else:
+        print("Digite uma opção válida!")
+
+    input("TECLE ENTER PARA CONTINUAR...")
