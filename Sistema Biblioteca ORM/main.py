1cfeb7ccea2567c57e61e733bcde10a79954e392
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-09-11 11:22:12 -0300

    Aluguel implementado

--- Sistema Biblioteca ORM/main.py
+++ Sistema Biblioteca ORM/main.py
@@ -1,9 +1,10 @@
 from database import Base, engine, Session
-from models import Livro
+from models import Livro, Cliente, Aluguel
 from datetime import date
 
 
 def configurarBanco():
+    Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
 
@@ -24,6 +25,7 @@ def cadastrarLivro():
     session.close()
     print("Livro Cadastrado:", novoLivro)
 
+
 def verLivros():
 
     print("Lista de Livros: ")
@@ -37,12 +39,84 @@ def verLivros():
         print(f"{livro.id} | {livro.titulo} | {livro.autor} | {livro.ano_publicacao}")
 
 
+def cadastrarCliente():
+
+    print("Cadastro de Novo Cliente")
+
+    nome = input("Digite o nome do cliente:")
+    email = input("Digite o email do cliente:")
+
+    cliente = Cliente(nome=nome, email=email)
+
+    session = Session()
+    session.add(cliente)
+    session.commit()
+    session.refresh(cliente)
+    session.close()
+
+    print(cliente)
+
+
+def verClientes():
+
+    print("Lista de Clientes:")
+
+    session = Session()
+    clientes = session.query(Cliente).all()
+    session.close()
+
+    print("ID | Nome | Email")
+    for cliente in clientes:
+        print(f"{cliente.id} | {cliente.nome} | {cliente.email}")
+
+
+def cadastrarAluguel():
+
+    verClientes()
+
+    idCliente = int(
+        input("Digite o id do cliente que está realizando o aluguel: "))
+
+    verLivros()
+
+    idLivro = int(input("Digite o id do livro que está sendo alugado: "))
+
+    aluguel = Aluguel(cliente_id=idCliente, livro_id=idLivro)
+
+    session = Session()
+    session.add(aluguel)
+    session.commit()
+    session.refresh(aluguel)
+    print("Aluguel Criado com Sucesso")
+    aluguel.mostrarInformacoes()
+    session.close()
+
+
+def verAlugueis():
+    print("Lista de Alugueis: ")
+
+    print("ID | Nome do Cliente | Titulo do Livro")
+
+    session = Session()
+    alugueis = session.query(Aluguel).all()
+    session.close()
+
+    for aluguel in alugueis:
+        print(f"{aluguel.id} | {aluguel.cliente.nome} | {aluguel.livro.titulo} ")
+
+
 while True:
 
     print("Menu: ")
     print('''
 1. Cadastrar Livro
 2. Ver Livros
+          
+3. Cadastrar Cliente
+4. Ver Clientes
+          
+5. Cadastrar Aluguel
+6. Ver Alugueis
 
 9. Configurar Banco de Dados
 0. Sair
@@ -53,6 +127,14 @@ while True:
         cadastrarLivro()
     elif op == "2":
         verLivros()
+    elif op == "3":
+        cadastrarCliente()
+    elif op == "4":
+        verClientes()
+    elif op == "5":
+        cadastrarAluguel()
+    elif op == "6":
+        verAlugueis()
     elif op == "9":
         configurarBanco()
     elif op == "0":
--- Sistema Biblioteca ORM/models.py
+++ Sistema Biblioteca ORM/models.py
@@ -10,12 +10,64 @@ class Livro(Base):
     autor = Column(String, nullable=False, default="Autor Não Identificado")
     ano_publicacao = Column(Integer, nullable=False)
 
-    def __repr__(self):
-        return f'''
+    alugueis = relationship("Aluguel", back_populates='livro')
+
+    def mostrarInformacoes(self):
+        print(f'''
 Informações do Livro:
 
 ID - {self.id}
 Titulo - {self.titulo}
 Autor - {self.autor}
 Ano Publicação - {self.ano_publicacao}
-'''
+''')
+
+    def __repr__(self):
+        return f"<Livro {self.titulo}>"
+
+
+class Cliente(Base):
+    __tablename__ = "cliente"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+
+    alugueis = relationship("Aluguel", back_populates="cliente")
+
+    def mostrarInformacoes(self):
+
+        print(f'''
+Informações do Cliente:
+              
+ID - {self.id}
+Nome - {self.nome}
+Email - {self.email}
+
+''')
+
+    def __repr__(self):
+        return f"<Cliente {self.nome}>"
+
+
+class Aluguel(Base):
+    __tablename__ = "aluguel"
+
+    id = Column(Integer, primary_key=True, index=True)
+    cliente_id = Column(Integer, ForeignKey("cliente.id"), nullable=False)
+    livro_id = Column(Integer, ForeignKey("livro.id"), nullable=False)
+
+    livro = relationship("Livro", back_populates="alugueis")
+    cliente = relationship("Cliente", back_populates="alugueis")
+
+    def __repr__(self):
+        return f"<Aluguel {self.livro.titulo} - {self.cliente.nome}>"
+
+    def mostrarInformacoes(self):
+        print(f'''
+Informações do Aluguel:
+              
+ID - {self.id}
+Titulo do Livro - {self.livro.titulo}
+Cliente - {self.cliente.nome}
+''')
